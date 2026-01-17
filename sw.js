adb4c32bb674aec5ad2ce855ec2cbf5be5ce055d
freddypenaloza67 <freddypenaloza67@gmail.com>
2026-01-17 18:58:41 -0400

    Initial commit - adding main file

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'gestorpro-v1.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://html2canvas.hertzen.com/dist/html2canvas.min.js',
+  'https://cdn-icons-png.flaticon.com/512/3050/3050525.png'
+];
+
+// Instalar service worker y cachear recursos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Interceptar peticiones y servir desde caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Limpiar cachés viejos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
